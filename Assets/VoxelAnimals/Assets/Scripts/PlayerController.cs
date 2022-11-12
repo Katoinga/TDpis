3d6b3454832e88c9eee397162e3229288a462c80
SergioSoncco <ssonccoc@unsa.edu.pe>
2022-11-12 08:25:34 -0500

    Level 2: environment

--- Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
+++ Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
 
-    public float movementSpeed = 1f;
-    public float rotSpeed = 50f;
+    public float movementSpeed = 3f;
+    public float rotSpeed = 100f;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
+    private float canJump = 0f;
     Animator anim;
     Rigidbody rb;
     
@@ -25,14 +28,15 @@ public class PlayerController : MonoBehaviour
     {
         if (isWandering == false) {
             StartCoroutine(Wander());
+            //anim.SetInteger("Walk", 0);
         }
         if(isRotRigth == true){
+            //anim.SetInteger("Walk", 0);
             transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
-            anim.SetInteger("Walk", 0);
         }
         if(isRotLeft == true){
+            //anim.SetInteger("Walk", 0);
             transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
-            anim.SetInteger("Walk", 0);
         }
         if(isWalking == true){
             transform.position += transform.forward * movementSpeed * Time.deltaTime;
@@ -45,7 +49,7 @@ public class PlayerController : MonoBehaviour
         int rotWait = Random.Range(1, 3);
         int rotLoR = Random.Range(1, 2);
         int walkWait = Random.Range(1, 2);
-        int walkTime = Random.Range(1, 4);
+        int walkTime = Random.Range(1, 7);
 
         isWandering = true;
 
@@ -65,5 +69,31 @@ public class PlayerController : MonoBehaviour
             isRotLeft = false;
         }
         isWandering = false;
+    }
+
+    void ControllPlayer()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            anim.SetInteger("Walk", 1);
+        }
+        else {
+            anim.SetInteger("Walk", 0);
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetButtonDown("Jump") && Time.time > canJump)
+        {
+                rb.AddForce(0, jumpForce, 0);
+                canJump = Time.time + timeBeforeNextJump;
+                anim.SetTrigger("jump");
+        }
     }
 }
